be27b712462d017df60d5c47f7bd65ef667f2dc8
hynjin <hynjin1004@naver.com>
2020-05-04 21:56:31 +0900

    month change

--- taxInvoice.py
+++ taxInvoice.py
@@ -5,24 +5,24 @@ import os
 import datetime
 
 erpWB = openpyxl.load_workbook('data/erp.xlsx')
-emailWB =  openpyxl.load_workbook('data/email.xlsx')
+emailWB =  openpyxl.load_workbook('data/info.xlsx')
 taxWB = openpyxl.load_workbook('data/origin.xlsx')
 
 erpWS = erpWB[erpWB.sheetnames[0]]
-emailWS = emailWB.active
+emailWS = emailWB.[emailWB.sheetnames[0]]
 taxWS = taxWB[taxWB.sheetnames[0]]
 
 # 이메일 사업자 번호를 키로 해쉬테이블
+emailDic = {'사업자번호':0, '이메일주소':1}
 emailAddress = dict()
 for i in range(2,emailWS.max_row+1):
     emailAddress[emailWS[i][0].value] = emailWS[i][1].value
     print(emailWS[i][0].value,emailAddress[emailWS[i][0].value])
 
 # erp 수금 기록 열 이름
-# 사업자번호, 법인명, 대표자, 업태, 종목, 사업장주소, 공급가액, 세액 사용
-erpDic = {'사업자번호':0,'법인명':1,'대표자':2,'업태':3,'종목':4,'사업장 주소':5,'공급가액':6,'세액':7}
+# 사업자번호, 법인명, 대표자, 업태, 종목, 사업장주소, 공급가액, 세액, 수금일자 사용
+erpDic = {'사업자번호':0,'법인명':1,'대표자':2,'업태':3,'종목':4,'사업장 주소':5,'공급가액':6,'세액':7, '수금일자':8}
 erpHash = dict() #열 이름을 키값으로 갖는 셀 위치
-erpName = []
 for c in range (0,erpWS.max_column):
     erpHash[erpWS[2][c].value] = erpWS[2][c].coordinate.strip('0123456789')
     print(erpWS[2][c].value,erpHash[erpWS[2][c].value])
@@ -52,7 +52,7 @@ for i in range (len(erpVal)):
         if j==0:
             temp.append('01')
         elif j==1:
-            temp.append(dt.strftime("%Y%m%d"))
+            temp.append(erpVal[i][erpDic['수금일']])
         elif j==2:
             temp.append(erpVal[i][erpDic['사업자번호']])
         elif j==4:
@@ -66,15 +66,16 @@ for i in range (len(erpVal)):
         elif j==8:
             temp.append(erpVal[i][erpDic['종목']])
         elif j==9:
-            temp.append(erpVal[i][8]) #이메일
+            temp.append(erpVal[i][9]) #이메일
         elif j==11:
             temp.append(erpVal[i][erpDic['공급가액']])
         elif j==12:
             temp.append(erpVal[i][erpDic['세액']])
         elif j==14:
-            temp.append(dt.strftime("%d"))
+            temp.append(erpVal[i][erpDic['수금일']][:2])
         elif j==15:
-            temp.append(dt.strftime("%m")+'월 CCTV용역료')
+            month = int(dt.strftime("%m"))
+            temp.append((month+11)%13+'월 CCTV용역료')
         elif j==19:
             temp.append(erpVal[i][erpDic['공급가액']])
         elif j==20:
